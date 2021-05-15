4c5e1b252db134e326fa70fb9396060aad253f57
eee23d <83975468+eee23d@users.noreply.github.com>
2021-05-16 01:04:17 +0300

    Update urls.py

--- djangoProject3/wingsApp/urls.py
+++ djangoProject3/wingsApp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.index,name='index'),
 path('register',views.register,name='register'),
 path('loginPage',views.loginPage,name='loginPage'),
+    path('logoutUser',views.logoutUser,name='logoutUser')
 
     ]
